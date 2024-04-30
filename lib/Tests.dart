3a45ca57798653ec0c4078318d52c4fb59ecfd64
4o3aXPEHb <polunosik3@gmail.com>
2024-04-30 20:37:19 +0400

    исправил текст тестов

--- lib/Tests.dart
+++ lib/Tests.dart
@@ -3,22 +3,22 @@ import 'models/TaskModel.dart';
 import 'models/TestModel.dart';
 
 List<Test> tests = [
-  Test(
-    guideId: -1,
-    title: "Квартирный вопрос",
-    tasks: [
-      Task(question: "Кто живет на дне океана?", ans: 1, answers: [
-        "губка для матья посуды",
-        "губка боб",
-        "губная гармошка"
-      ]),
-      Task(question: "Кто НЕ живет на дне океана?", ans: 0, answers: [
-        "губка для матья посуды",
-        "губка боб",
-        "патрик"
-      ]),
-    ]
-  ),
+  // Test(
+  //   guideId: -1,
+  //   title: "Квартирный вопрос",
+  //   tasks: [
+  //     Task(question: "Кто живет на дне океана?", ans: 1, answers: [
+  //       "губка для матья посуды",
+  //       "губка боб",
+  //       "губная гармошка"
+  //     ]),
+  //     Task(question: "Кто НЕ живет на дне океана?", ans: 0, answers: [
+  //       "губка для матья посуды",
+  //       "губка боб",
+  //       "патрик"
+  //     ]),
+  //   ]
+  // ),
   Test(
       guideId: 0,
       title: guides[0].title,
